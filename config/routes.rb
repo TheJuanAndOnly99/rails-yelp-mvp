c5b90af4601037c27ebe4432d51486bf92149f12
Juan Estrella <juan.estrella137@gmail.com>
2020-03-11 16:45:18 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants, except: [:destroy, :update] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:show, :edit, :update, :delete, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
